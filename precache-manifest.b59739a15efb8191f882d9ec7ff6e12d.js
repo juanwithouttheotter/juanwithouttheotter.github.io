64e2ac27ad1b55f791d4234b7ba09165ed5b633c
Juan Osorio <juan.michael.osorio@gmail.com>
2020-08-26 15:40:55 -0500

    Updates

---
+++ precache-manifest.b59739a15efb8191f882d9ec7ff6e12d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6624278a7cb7308754536f3b408165ab",
+    "url": "/re-active-portfolio/index.html"
+  },
+  {
+    "revision": "301eddb74f95abbbe7af",
+    "url": "/re-active-portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3d16372a03415f7c4179",
+    "url": "/re-active-portfolio/static/css/main.ff083bdb.chunk.css"
+  },
+  {
+    "revision": "301eddb74f95abbbe7af",
+    "url": "/re-active-portfolio/static/js/2.d170ba5e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/re-active-portfolio/static/js/2.d170ba5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d16372a03415f7c4179",
+    "url": "/re-active-portfolio/static/js/main.9981eab2.chunk.js"
+  },
+  {
+    "revision": "e7b6e2bcf50b23f07351",
+    "url": "/re-active-portfolio/static/js/runtime-main.2efad38d.js"
+  },
+  {
+    "revision": "37bee35e6fbe68209c4a036991d78af9",
+    "url": "/re-active-portfolio/static/media/profile-photo-me.37bee35e.jpg"
+  }
+]);
