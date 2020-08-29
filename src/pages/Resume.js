6dadcb34e953feea7bccdad19cd7a834bf1ac1fd
Juan Osorio <juan.michael.osorio@gmail.com>
2020-08-29 09:44:00 -0500

    creating links and formatting

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import Container from '../components/Container';
+import Col from '../components/Col';
+import Row from '../components/Row';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload, faDna } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+// import { } from '@fortawesome/free-solid-svg-icons';
+
 
 function Resume() {
-    return(
-        <div>
-            resume
-        </div>
+    return (
+        <Container fluid="true" addclass=" my-5 text-center">
+            <Row>
+                
+                <Col size="md">
+                    <a href="https://www.linkedin.com/in/juan-osorio-5496201b4/">
+                    <FontAwesomeIcon icon={faLinkedin} size="8x"/>
+                    </a>
+                </Col>
+            </Row>
+            <div className="my-5"></div>
+            <Row>    
+                <Col size="md">
+                    <a href="">
+                    <FontAwesomeIcon icon={faDownload} size="8x" />
+                    </a>
+                </Col>
+                
+            </Row>
+            <div className="my-5"></div>
+            <Row>
+                
+                <Col size="md">
+                    <a href="https://github.com/juanwithouttheotter">
+                    <FontAwesomeIcon icon={faGithub} size="8x" />
+                    </a>
+                </Col>
+            </Row>
+            <div className="my-5"></div>
+            <Row>
+                <Col size="md">
+                    <a href="https://juanwithouttheotter.github.io/Movie-Weather-Solutions/">
+                    <FontAwesomeIcon icon={faDna} size="8x" />
+                    </a>
+                </Col>
+                
+            </Row>
+        </Container>
     )
 }
 
