cd0f1d47454e6454708fbc54838530ab710fd9ca
Juan Osorio <juan.michael.osorio@gmail.com>
2020-10-25 15:00:51 -0500

    Updates

---
+++ precache-manifest.7b64715f1b48d837e4e85452d60d2c96.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e55a918c560cb6c7c33f43e457d3e3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "373f6e97fdcfbb012ff5",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c92a1b1b444756864bc8",
+    "url": "/static/css/main.9c5d211d.chunk.css"
+  },
+  {
+    "revision": "373f6e97fdcfbb012ff5",
+    "url": "/static/js/2.8bbe8ed8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8bbe8ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92a1b1b444756864bc8",
+    "url": "/static/js/main.b033402c.chunk.js"
+  },
+  {
+    "revision": "0e2ec18f7dd4deea235d",
+    "url": "/static/js/runtime-main.82ba793a.js"
+  },
+  {
+    "revision": "1c52c1c35ce048d3142dbf08530ea54d",
+    "url": "/static/media/Marvelous-Markdown-Generator-image.1c52c1c3.png"
+  },
+  {
+    "revision": "0d2e60eab96e84d375250f754fe01ced",
+    "url": "/static/media/Movie-weather-solutions-image.0d2e60ea.png"
+  },
+  {
+    "revision": "edadd8ecb4372c344359498dd324104c",
+    "url": "/static/media/Place-holder.edadd8ec.png"
+  },
+  {
+    "revision": "ee8a885c3552ffcc71d877af3ac909d9",
+    "url": "/static/media/celeb-o-knowledge-image.ee8a885c.png"
+  },
+  {
+    "revision": "24ff80c63462de61b21846ea2b82687a",
+    "url": "/static/media/morndusk-planner-image.24ff80c6.png"
+  },
+  {
+    "revision": "dcd99a4fadabe2fb9806acb815991556",
+    "url": "/static/media/pass-gen-image.dcd99a4f.png"
+  },
+  {
+    "revision": "37bee35e6fbe68209c4a036991d78af9",
+    "url": "/static/media/profile-photo-me.37bee35e.jpg"
+  },
+  {
+    "revision": "cd6fd2a8c609a6095e57abd8f105e5b9",
+    "url": "/static/media/resume-osorio-general.cd6fd2a8.pdf"
+  },
+  {
+    "revision": "8d4e072a3f0acf5a88f3bebb3fe81724",
+    "url": "/static/media/weather-today-or-tomorrow-image.8d4e072a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/re-active-portfolio/precache-manifest.d5aae3870fc1ed712ab3e11fdabca2e4.js"
+  "/precache-manifest.7b64715f1b48d837e4e85452d60d2c96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/re-active-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
