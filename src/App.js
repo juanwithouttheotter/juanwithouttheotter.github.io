bcf089e38959edf71192b6158e2e02a8e6cdeb2c
Juan Osorio <juan.michael.osorio@gmail.com>
2020-11-07 17:18:43 -0600

    adjusting routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import About from "./pages/About";
@@ -14,8 +14,8 @@ function App() {
       <Navbar />
       <Route exact path="/" component={About} />
       <Route exact path="/portfolio" component={Portfolio} />
+      
       <Route exact path="/resume" component={Resume} />
-
       <Footer />
     </Router>
   );
