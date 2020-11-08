52bf63846a7d4da6e19f655bccad021c65a38162
Juan Osorio <juan.michael.osorio@gmail.com>
2020-11-07 18:20:59 -0600

    cleaning up resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,49 +3,34 @@ import Container from '../components/Container';
 import Col from '../components/Col';
 import Row from '../components/Row';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload, faDna } from '@fortawesome/free-solid-svg-icons';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 // import { } from '@fortawesome/free-solid-svg-icons';
-import PDF from '../assets/resume-osorio-general.pdf';
+import PDF from '../assets/Osorio-Juan-Resume.pdf';
 
 
 function Resume() {
     return (
         <Container fluid="true" addclass=" my-5 text-center">
             <Row>
-                
-                <Col size="md">
-                    <a href="https://www.linkedin.com/in/juan-osorio-5496201b4/">
-                    <FontAwesomeIcon icon={faLinkedin} size="8x"/>
+                <Col size="sm">
+                    <a href="https://www.linkedin.com/in/juanosorio12/" title="linkedin" >
+                        <FontAwesomeIcon icon={faLinkedin} size="4x" />
                     </a>
                 </Col>
-            </Row>
-            <div className="my-5"></div>
-            <Row>    
-                <Col size="md">
-                    <a href={PDF} download target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faDownload} size="8x" />
+                <div className="my-5"></div>
+                <Col size="sm">
+                    <a href={PDF} download target="_blank" rel="noopener noreferrer" title="download resume">
+                        <FontAwesomeIcon icon={faDownload} size="4x" />
                     </a>
                 </Col>
-                
-            </Row>
-            <div className="my-5"></div>
-            <Row>
-                
-                <Col size="md">
-                    <a href="https://github.com/juanwithouttheotter">
-                    <FontAwesomeIcon icon={faGithub} size="8x" />
+                <div className="my-5"></div>
+                <Col size="sm">
+                    <a href="https://github.com/juanwithouttheotter" title="Github">
+                        <FontAwesomeIcon icon={faGithub} size="4x" />
                     </a>
                 </Col>
-            </Row>
-            <div className="my-5"></div>
-            <Row>
-                <Col size="md">
-                    <a href="https://juanwithouttheotter.github.io/Movie-Weather-Solutions/">
-                    <FontAwesomeIcon icon={faDna} size="8x" />
-                    </a>
-                </Col>
-                
+                <div className="my-5"></div>
             </Row>
         </Container>
     )
