efcfcd67a472e0b6388b11b41dff1aa0e43112c5
Juan Osorio <juan.michael.osorio@gmail.com>
2023-05-09 14:08:24 -0500

    Update router asnd linter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import About from "./pages/About";
-import Portfolio from "./pages/Portfolio";
-import Resume from "./pages/Resume";
-
-
+import { About, Portfolio, Resume } from "./pages";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
-      <Route exact path="/" component={About} />
-      <Route exact path="/portfolio" component={Portfolio} />
-      
-      <Route exact path="/resume" component={Resume} />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
       <Footer />
-    </Router>
+    </>
   );
 }
 
